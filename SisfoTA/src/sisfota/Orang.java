d7a7ba9fa50b9341fa7439f97f0779e39446f26c
ZulfaUmi <ulfawardanii@gmail.com>
2016-03-09 14:56:15 +0700

    abstract orang

---
+++ SisfoTA/src/sisfota/Orang.java
@@ -0,0 +1,23 @@
+package sisfota;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
+
+/**
+ *
+ * @author ZULFA UMI WARDANI
+ */
+
+public abstract class Orang {
+    public String nama;
+    public long ktp;
+    public date tglLahir;
+    public String tempatLahir;
+    public String agama;
+}
