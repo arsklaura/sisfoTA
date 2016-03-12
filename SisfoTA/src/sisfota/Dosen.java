e58c2b6be99dfaf89c625985b0ebacddbb011e07
ZulfaUmi <ulfawardanii@gmail.com>
2016-03-12 11:39:48 +0700

    abstract orang

--- SisfoTA/src/sisfota/Dosen.java
+++ SisfoTA/src/sisfota/Dosen.java
@@ -16,22 +16,31 @@ public class Dosen {
     private int nTopikTA;
     private int maxTopikTA;
     private long NIP;
-    
+    private String tempJudul;
+    private String setJudulTA;
+    private int i;
+    private null[] anggota;
+    private null[] anggota;
     public Dosen(){};
     public void createKelompkTA(int i){
         for ( i=0; i<=5; i++) {
             kelompokTA[i]= getAnggota();
         }
     }
+    
     public void revisiJudulTA (){
-        System.out.println("Judul direvisi ");
-    };
-    public removeKelompokTA (){
+        tempJudul = TugasAkhir.getjudulTA();
+        System.out.println("masukkan judul baru : "+setJudulTA());
+    }
+    public void removeKelompokTA (){
         anggota[i]= null;
         System.out.println(" Kelompok telah dihapus ");
     }
-}
-  
-    
-    
-}
+
+    private String getjudulTA() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private String setJudulTA() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
---
+++ SisfoTA/src/sisfota/TugasAkhir.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisfota;
+
+/**
+ *
+ * @author ZULFA UMI WARDANI
+ */
+class TugasAkhir {
+
+    static String getjudulTA() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
